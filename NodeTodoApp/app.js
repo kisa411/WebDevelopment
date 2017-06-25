b0a93fd09b967380babfafb8097bf0b9ad5c925a
Emily Koh <kisa411@Emilys-MacBook-Pro-3.local>
2017-06-25 15:58:57 -0500

    created vue isntance

--- NodeTodoApp/app.js
+++ NodeTodoApp/app.js
@@ -25,4 +25,15 @@ apiController(app);
 
 app.listen(port);
 
+new Vue({
+    // target div with an id of 'todos'
+    el: '#todos',
+    // register any values or collections that hold data for the application
+    data:{},
+    // anything within the ready function will run when the application loads
+    ready: function() {
 
+    },
+    // methods to use in the application are registered here
+    methods: {}
+});
