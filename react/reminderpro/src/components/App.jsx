fe1bcdd3ed27fc0f8da1ffd8e91d6543ceac136f
Emily Koh <kisa411@gmail.com>
2017-09-05 16:37:26 -0500

    add redux to flow

--- react/reminderpro/src/components/App.jsx
+++ react/reminderpro/src/components/App.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder() {
+        console.log('this', this);
+        this.props.addReminder(this.state.text);
+    }
+
     render() {
         return (
             <div className="App">
@@ -12,11 +28,13 @@ class App extends Component {
                         <input
                             className="form-control"
                             placeholder="I have to..." 
+                            onChange={event => this.setState({text:event.target.value})}
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={ ()=> this.addReminder()}
                     >
                         Add Reminder
                     </button>
@@ -26,4 +44,9 @@ class App extends Component {
     }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({addReminder}, dispatch);
+}
+// bindActionCreators 
+
+export default connect(null, mapDispatchToProps)(App);
